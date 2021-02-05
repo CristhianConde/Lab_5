5b368168c2eaf26ca9e8f8339a962a16c107cfc4
CCCS <GitHubEmail@something.com>
2021-02-04 21:38:38 -0600

    editing Alaska 2

--- Script.R
+++ Script.R
@@ -1,4 +1,21 @@
 library(tidyverse)
-list.files()
+library(readxl)
+library(tidyverse)
+
+#Editing Alaska
 
 a <- readxl::read_excel(path = "Alaska.xlsx", sheet = 1)
+
+head(a)
+str(a)
+
+yr <- as.character(a$year)
+mo <- as.character(a$month)
+mo <- str_c("0",mo, sep = "")
+
+yr_mo <- str_c(yr,mo,sep = "-")
+head(yr_mo)
+
+a$date <- yr_mo
+
+write.csv(x = a, file = "Alaska_fixed.csv", row.names = F)
